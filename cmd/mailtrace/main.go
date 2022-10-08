f250bafc9ca8317159ea1c9f3007ce1314283c56
Arran Ubels <arran4@gmail.com>
2022-10-08 23:11:26 +1100

    Stream

--- cmd/mailtrace/main.go
+++ cmd/mailtrace/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
@@ -13,7 +14,7 @@ import (
 )
 
 var (
-	//inputType = flag.String("input-type", "mbox", "The input type")
+	inputType = flag.String("input-type", "mailfile", "The input type")
 	inputFile = flag.String("input", "-", "Input file or - for stdin")
 	//outputType = flag.String("output-type", "mbox", "The input type")
 	outputFile = flag.String("output", "-", "Output file or - for stdin")
@@ -26,9 +27,54 @@ type MailBody interface {
 	FormName() string
 }
 
+type MailBodyFromPart struct {
+	Body *bytes.Buffer
+	Part *multipart.Part
+}
+
+func (m *MailBodyFromPart) Read(p []byte) (n int, err error) {
+	return m.Body.Read(p)
+}
+
+func (m *MailBodyFromPart) Header() textproto.MIMEHeader {
+	return m.Part.Header
+}
+
+func (m *MailBodyFromPart) FileName() string {
+	return m.Part.FileName()
+}
+
+func (m *MailBodyFromPart) FormName() string {
+	return m.Part.FormName()
+}
+
+var _ MailBody = (*MailBodyFromPart)(nil)
+
+type MailBodyGeneral struct {
+	Body *bytes.Buffer
+}
+
+func (m *MailBodyGeneral) Read(p []byte) (n int, err error) {
+	return m.Body.Read(p)
+}
+
+func (m *MailBodyGeneral) Header() textproto.MIMEHeader {
+	return map[string][]string{}
+}
+
+func (m *MailBodyGeneral) FileName() string {
+	return ""
+}
+
+func (m *MailBodyGeneral) FormName() string {
+	return ""
+}
+
+var _ MailBody = (*MailBodyGeneral)(nil)
+
 type MailWithSource struct {
 	MailHeader mail.Header
-	MailBodies []*MailBody
+	MailBodies []MailBody
 	SourceType string
 	SourceFile string
 }
@@ -36,32 +82,35 @@ type MailWithSource struct {
 func main() {
 	flag.Parse()
 	mails := []*MailWithSource{}
-	switch *inputFile {
-	case "-":
-		nm, err := ReadMbox(os.Stdin, "mbox", *inputFile)
-		if err != nil {
-			log.Panicln(err)
+	switch *inputType {
+	case "mailfile":
+		switch *inputFile {
+		case "-":
+			nm, err := ReadMailStream(os.Stdin, *inputType, *inputFile)
+			if err != nil {
+				log.Panicln(err)
+			}
+			mails = append(mails, nm...)
+		default:
+			nm, err := ReadMailFile(*inputType, *inputFile)
+			if err != nil {
+				log.Panicln(err)
+			}
+			mails = append(mails, nm...)
 		}
-		mails = append(mails, nm...)
-	default:
-		nm, err := ReadMboxFile("mbox", *inputFile)
-		if err != nil {
-			log.Panicln(err)
-		}
-		mails = append(mails, nm...)
 	}
 }
 
-func ReadMboxFile(fType, fName string) ([]*MailWithSource, error) {
+func ReadMailFile(fType, fName string) ([]*MailWithSource, error) {
 	f, err := os.OpenFile(fName, os.O_RDONLY, 0644)
 	if err != nil {
 		return nil, fmt.Errorf("reading Mbox %s: %w", fName, err)
 	}
 	defer f.Close()
-	return ReadMbox(f, fType, fName)
+	return ReadMailStream(f, fType, fName)
 }
 
-func ReadMbox(f io.Reader, fType string, fName string) ([]*MailWithSource, error) {
+func ReadMailStream(f io.Reader, fType string, fName string) ([]*MailWithSource, error) {
 	ms := []*MailWithSource{}
 	for {
 		msg, err := mail.ReadMessage(f)
@@ -71,7 +120,7 @@ func ReadMbox(f io.Reader, fType string, fName string) ([]*MailWithSource, error
 		if msg == nil {
 			return ms, nil
 		}
-		mb := []*MailBody{}
+		mb := []MailBody{}
 		ct := msg.Header.Get("Content-Type")
 		mt, mtp, err := mime.ParseMediaType(ct)
 		switch mt {
@@ -81,15 +130,21 @@ func ReadMbox(f io.Reader, fType string, fName string) ([]*MailWithSource, error
 			if err != nil {
 				return nil, fmt.Errorf("reading message %d part %d from Mbox %s: %w", len(ms)+1, len(mb)+1, fName, err)
 			}
-			mb = append(mb, &MailBody{
-				p,
+			b := bytes.NewBuffer(nil)
+			if _, err := io.Copy(b, p); err != nil {
+				return nil, fmt.Errorf("reading body of message %d part %d from Mbox %s: %w", len(ms)+1, len(mb)+1, fName, err)
+			}
+			mb = append(mb, &MailBodyFromPart{
+				Body: b,
+				Part: p,
 			})
-			p.
 		default:
-			mb = append(mb, &MailBody{
-				&multipart.Part{
-
-				},
+			b := bytes.NewBuffer(nil)
+			if _, err := io.Copy(b, msg.Body); err != nil {
+				return nil, fmt.Errorf("reading body of message %d part %d from Mbox %s: %w", len(ms)+1, len(mb)+1, fName, err)
+			}
+			mb = append(mb, &MailBodyGeneral{
+				Body: b,
 			})
 		}
 		ms = append(ms, &MailWithSource{
