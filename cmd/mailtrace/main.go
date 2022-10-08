f6bfe81aa21c6e30309eb30309c2ecf6398b3521
Arran Ubels <arran4@gmail.com>
2022-10-08 18:46:28 +1100

    ..

--- cmd/mailtrace/main.go
+++ cmd/mailtrace/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "flag"
+
+var (
+	inputType = flag.String("input-type", "mbox", "The input type")
+	inputFile = flag.String("input", "-", "Input file or - for stdin")
+	outputType =
+)
+
 func main() {
 
 }
