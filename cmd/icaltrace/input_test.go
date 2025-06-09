0b253dc9979c4c2cc706c500a8c93b995364fb78
Arran Ubels <arran4@gmail.com>
2025-06-09 23:13:06 +1000

    Update icaltrace help

---
+++ cmd/icaltrace/input_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+func captureOutput(f func()) string {
+	r, w, _ := os.Pipe()
+	old := os.Stdout
+	os.Stdout = w
+	f()
+	w.Close()
+	os.Stdout = old
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	r.Close()
+	return buf.String()
+}
+
+func TestPrintInputHelpContainsIcal(t *testing.T) {
+	out := captureOutput(PrintInputHelp)
+	if !strings.Contains(out, "ical") {
+		t.Errorf("expected help to contain 'ical' but got %q", out)
+	}
+}
